ffa17efacf6de17b88be4b54818d66ca04c819dd
restapitesting <deepak071984@gmail.com>
2026-02-24 07:44:54 +0530

    push results

--- tests/sampleTest_updateResultOnSameSuite.spec.ts
+++ tests/sampleTest_updateResultOnSameSuite.spec.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 import { LoginPage } from '../pages/loginpage';
 import { HomePage } from '../pages/homepage';
 import testData from '../test-data/credentials.json';
+import { Console } from 'console';
 
 
 // ------------------- Test Cases -------------------
@@ -13,6 +14,8 @@ test.describe('Sample test cases', () => {
 
   test('Login with valid credentials', async ({ page }) => {
     console.log("First Testcase passed");
+    console.log("test results");
+    
     // const loginpage = new LoginPage(page);
     // await loginpage.goto();
     // await loginpage.login(process.env.USER_NAME!, process.env.PASSWORD!);
